810b2829d2bc0da2baa8230742e1baea33a6e4ed
DanishSyaz <danishsyazanie.muhamad@gmail.com>
2026-04-23 21:11:42 +0800

    Replace everything with new files

--- inventoria-fullstack/backend/src/main/java/com/inventoria/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.inventoria.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
